0345927cf0a3cfd8f2f7b4258734d7bddeff598e
Felipe Bueno de Paula <gvv.fel@gmail.com>
2020-11-24 00:39:40 -0300

    feat: render bird and base ground

--- src/components/Canvas/index.ts
+++ src/components/Canvas/index.ts
@@ -1,18 +1,72 @@
 import Component from '../Component';
+
 import BackgroundDay from '../../assets/sprites/background-day.png';
+import YellowBird from '../../assets/sprites/yellowbird-midflap.png';
+import Base from '../../assets/sprites/base.png';
 
 export default class Canvas extends Component {
     private _ctx: CanvasRenderingContext2D | null;
+
     constructor() {
         super(
             document.createElement('canvas'),
             '/_dist_/components/Canvas/index.css', // TODO resolve this path
         );
-        this.setCanvasSize(288, 512);
+        this.setCanvasSize(288, 512); // TODO provide with css variables
         this._ctx = this.elem<HTMLCanvasElement>().getContext('2d');
-        this.loadBackground();
+        // TODO extract within Components and recieve on constructor (render by frames -> requestAnimationFrame)
+        this.renderBackground();
+        this.renderBase();
+        this.renderBird();
     }
-    private async loadBackground() {
+
+    private async renderBird() {
+        const birdImage = new Image();
+        birdImage.src = YellowBird;
+        return new Promise((resolve) => {
+            birdImage.onload = () => {
+                resolve(
+                    this._ctx?.drawImage(
+                        birdImage,
+                        0,
+                        0,
+                        34,
+                        24,
+                        50,
+                        50,
+                        34,
+                        24,
+                    ),
+                );
+            };
+        });
+    }
+
+    private async renderBase() {
+        const { height: canvasHeight } = this.elem<HTMLCanvasElement>();
+        const destY = canvasHeight - 112;
+        const base = new Image();
+        base.src = Base;
+        return new Promise((resolve) => {
+            base.onload = () => {
+                resolve(
+                    this._ctx?.drawImage(
+                        base,
+                        0,
+                        0,
+                        336,
+                        112,
+                        0,
+                        destY,
+                        336,
+                        112,
+                    ),
+                );
+            };
+        });
+    }
+
+    private async renderBackground() {
         const { width, height } = this.elem<HTMLCanvasElement>();
         const background = new Image(width, height);
         background.src = BackgroundDay;

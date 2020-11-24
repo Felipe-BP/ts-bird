0a923fe416b1d397cb4b9d389415a818ac1a9430
Felipe Bueno de Paula <gvv.fel@gmail.com>
2020-11-24 00:16:17 -0300

    feat: render background on canvas

--- src/components/Canvas/index.ts
+++ src/components/Canvas/index.ts
@@ -8,9 +8,35 @@ export default class Canvas extends Component {
             document.createElement('canvas'),
             '/_dist_/components/Canvas/index.css', // TODO resolve this path
         );
+        this.setCanvasSize(288, 512);
         this._ctx = this.elem<HTMLCanvasElement>().getContext('2d');
-        const background = new Image(288, 512);
+        this.loadBackground();
+    }
+    private async loadBackground() {
+        const { width, height } = this.elem<HTMLCanvasElement>();
+        const background = new Image(width, height);
         background.src = BackgroundDay;
-        this._ctx?.drawImage(background, 0, 0, 288, 512, 0, 0, 288, 512);
+        return new Promise((resolve) => {
+            background.onload = () => {
+                resolve(
+                    this._ctx?.drawImage(
+                        background,
+                        0,
+                        0,
+                        width,
+                        height,
+                        0,
+                        0,
+                        width,
+                        height,
+                    ),
+                );
+            };
+        });
+    }
+
+    private setCanvasSize(width: number, height: number) {
+        this.elem<HTMLCanvasElement>().width = width;
+        this.elem<HTMLCanvasElement>().height = height;
     }
 }

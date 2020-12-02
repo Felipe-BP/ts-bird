490c87170483082d1ce79a7481087d3a83fd31ef
Felipe Bueno de Paula <gvv.fel@gmail.com>
2020-12-02 01:16:57 -0300

    feat: logic to create bird flap animation
    - Change bird asset to blue, because it's typescript :D

--- src/components/TS-Bird/index.ts
+++ src/components/TS-Bird/index.ts
@@ -1,40 +1,69 @@
 import type { ObjectState } from '../../interfaces/object-state.interface';
 import type { RenderedComponent } from '../../interfaces/rendered-component.interface';
 
-import YellowBird from '../../assets/sprites/yellowbird-midflap.png';
+import { FrameRateUtil } from '../../utils/frame-rate.util';
 
+import BlueBirdUp from '../../assets/sprites/bluebird-upflap.png';
+import BlueBirdMid from '../../assets/sprites/bluebird-midflap.png';
+import BlueBirdDown from '../../assets/sprites/bluebird-downflap.png';
+
+type BirdState = ObjectState & { birdFlapStates: Array<string> };
 export class TSBird implements RenderedComponent {
-    DEFAULT_STATE: ObjectState = {
+    DEFAULT_STATE: BirdState = {
         image: null,
         sourceX: 0,
         sourceY: 0,
         sourceW: 34,
         sourceH: 24,
-        destX: 50,
-        destY: 50,
+        destX: 40,
+        destY: 90,
         destW: 34,
         destH: 24,
+        birdFlapStates: [BlueBirdMid, BlueBirdDown, BlueBirdUp],
     };
-    private _state: ObjectState = this.DEFAULT_STATE;
+    private _state: BirdState = this.DEFAULT_STATE;
     private gravity = 0.25;
     private velocity = 1;
 
-    get state(): ObjectState {
+    get state(): BirdState {
         return this._state;
     }
 
-    render(ctx: CanvasRenderingContext2D | null): void {
+    async render(ctx: CanvasRenderingContext2D | null): Promise<void> {
         if (this.state.image) {
+            this.state.image = await this.getBirdFlapImage();
             return this.drawOnCanvas(ctx);
         }
+
         const birdImage = new Image();
-        birdImage.src = YellowBird;
+        birdImage.src = BlueBirdMid;
         new Promise((resolve) => {
             birdImage.onload = () => {
                 this.state.image = birdImage;
                 resolve(this.drawOnCanvas(ctx));
             };
         });
+    }
+
+    async getBirdFlapImage(): Promise<HTMLImageElement> {
+        const birdImage = new Image();
+        birdImage.src = this.defineFlapImage();
+        return new Promise((resolve) => {
+            birdImage.onload = () => resolve(birdImage);
+        });
+    }
+
+    defineFlapImage(): string {
+        const updateInterval = 10;
+        const passedInterval = FrameRateUtil.frame % updateInterval === 0;
+
+        if (passedInterval) {
+            const index =
+                FrameRateUtil.frame % this.state.birdFlapStates.length;
+            return this.state.birdFlapStates[index];
+        }
+
+        return (this.state.image as HTMLImageElement).src;
     }
 
     drawOnCanvas(ctx: CanvasRenderingContext2D | null): void {
